ef2be0448ffed111d9a9cc3e348d732b8dacb22b
Colin Scott-Fleming <colinsf@arizona.edu>
2013-08-05 16:49:17 -0700

    Reorganized some test cases.

---
+++ Geeks Who DrankTests/EventManagerTests.h
@@ -0,0 +1,13 @@
+//
+//  EventManagerTests.h
+//  Geeks Who Drank
+//
+//  Created by Colin Scott-Fleming on 8/5/13.
+//  Copyright (c) 2013 csfds. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface EventManagerTests : SenTestCase
+
+@end
